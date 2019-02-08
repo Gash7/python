d5f8f97c0047034224eaf46ffdd53873f3e79c1a
Gash7 <avgore@gmail.com>
2019-02-08 19:01:33 +0530

    Python Basics

---
+++ write.py
@@ -0,0 +1,19 @@
+file=open(r'C:\Users\AMIT_GORE\Desktop\\abc.txt','w+')
+abc=file.write('Be formal to the people around you!!!!')
+print(abc)
+file.close()
+
+
+
+
+li=[]
+file=open(r'C:\Users\AMIT_GORE\Desktop\\abc.txt','w+')
+abc=file.readlines()
+print(abc)
+file.close()
+
+#file=open(r'C:\Users\AMIT_GORE\Desktop\\abc.txt','w')
+#file.write('Hi ashish, Be Professional Do not iretate people')
+#file.close()
+
+
